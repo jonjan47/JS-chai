0161f2235d39ef622921b0a07ef5990eea4ed005
jonjan47 <148625742+jonjan47@users.noreply.github.com>
2024-01-20 03:06:50 +0000

    conversionOpreaters

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,7 +3,7 @@ let accountEmail = "gujjar@gamil.com"
 var accountPassword = "2344"
 accountCity = "sahiwal"
 /*
-parfar var variable not use because its scope level issuse
+parfar not to use var because its scope level issuse
 */
 // accountId = 77
 // console.log(accountId); const variable is not change
---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,47 @@
+//let score = 34
+//let score = "345abd"
+//let score = true
+//let score = null
+let score = undefined
+//console.log(typeof score);
+// console.log(typeof(score));
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*"34"=>34
+"34acv"= NaN
+true=>1; false=> 0
+""=>false; "asd"=>true*/
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+//************************Oprations***********************
+let value = 5
+let negValue =-value
+// console.log(negValue);
+
+/*
+console.log(2+3);
+console.log(3-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+ */
+let str1 ="khuram"
+let str2 =" gujjar"
+let str3 = str1 + str2
+//console.log(str3);
+// console.log("1" +2);
+// console.log(1 +"2");
+// console.log("1" +2 +2);
+// console.log(1 + 2 +"2");
+//console.log(+true);
+//console.log(+"5");
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
