466db67b3fa29aedddeccd332af34dc9685b5ae0
jonjan47 <148625742+jonjan47@users.noreply.github.com>
2024-01-20 02:05:02 +0000

    variabes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let accountEmail = "gujjar@gamil.com"
+var accountPassword = "2344"
+accountCity = "sahiwal"
+/*
+parfar var variable not use because its scope level issuse
+*/
+// accountId = 77
+// console.log(accountId); const variable is not change
+accountEmail = "ali@gamil.com"
+accountPassword = "0987"
+accountCity = "lahore"
+
+console.table([accountEmail, accountPassword, accountCity]);
