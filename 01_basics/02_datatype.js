b48c78b2d139f8c01efa79009c0658ae7aeeae54
jonjan47 <148625742+jonjan47@users.noreply.github.com>
2024-01-20 02:23:16 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+// alert(3+3) we are using nodejs not browser
+
+//console.log(3+
+  //  6);code readability be high
+    //console.log("khuram");
+
+    let name = "gujjar"
+    let age = 24
+    let isLoggedIn = true
+    let state;
+    /*
+    1 number =>2 to power 53
+    2 bigint 
+    string=>""
+    boolean=> true/false
+    null=>standalone value
+    undefined 
+    symbol=>unique
+    */
+   
+    //object
+    console.log(typeof null);
